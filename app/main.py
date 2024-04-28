9532820c9b8ad2e58f98bb313235fafc51c0d868
Argyris <arkatsy@gmail.com>
2024-04-28 04:31:48 +0300

    feat: add simple http res

--- app/main.py
+++ app/main.py
@@ -5,12 +5,19 @@ server_port = 4221
 
 def main():
     server_socket = socket.create_server((server_ip, server_port))
-    client_socket, client_address = server_socket.accept()
-    print(f"client_socket: {client_socket}")
-    print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+    conn, addr = server_socket.accept()
+    data = conn.recv(1024).decode()
 
+    if data:
+        res = create_response()
+        conn.send(res)
 
+def create_response(opts = {}):
+    protocol = "HTTP/1.1"
+    status_code = opts.get("status_code", 200)
+    status = opts.get("status", "OK")
 
+    return bytes(f"{protocol} {status_code} {status}\r\n\r\n", "utf-8")
 
 if __name__ == "__main__":
     main()
